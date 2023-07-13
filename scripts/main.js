75ef940043058adab90081855e96174a688d0950
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2023-07-13 17:06:08 +0200

    exercise done

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,18 @@
 //Sets the target date to countdown from.
 const targetDate = new Date(2023, 6, 14, 9, 30);
 
-//Views in consolle "targetDate".
-console.log(targetDate);
+//Variable for html element where to display the time left
+const timeLeftElement = document.getElementById("time_left");
 
+//Views in console "targetDate".
+console.log(targetDate);
 
 //SetInterval function calls dateDifference function as argument.
 setInterval(dateDifference, 100);
 
+/**
+ * Calculates the time difference between present time and "targetDate".
+ */
 function dateDifference() {
 
     //Creates a new Date object with the day of now.
@@ -25,23 +30,24 @@ function dateDifference() {
 
     const daysLeftRm = timeToLesson % (24*60*60*1000);
 
-    const hoursLeft =  Math.floor(daysLeftRm / (60*60*1000));
+    const hoursLeft = Math.floor(daysLeftRm / (60*60*1000));
 
-    const hoursLeftRm =  timeToLesson % (60*60*1000);
+    const hoursLeftRm = timeToLesson % (60*60*1000);
 
-    const minutesLeft =   Math.floor(hoursLeftRm / (60*1000));
+    const minutesLeft = Math.floor(hoursLeftRm / (60*1000));
 
-    const minutesLeftRm =   timeToLesson % (60*1000);
+    const minutesLeftRm = timeToLesson % (60*1000);
 
-    const secondsLeft =    Math.floor(minutesLeftRm / 1000);
+    const secondsLeft = Math.floor(minutesLeftRm / 1000);
 
     //Creates a variable formatting the total time left (d:h:m:s).
     const formattedTimeToLesson = `${daysLeft}:${hoursLeft}:${minutesLeft}:${secondsLeft}`;
 
-    //Views in consolle the formatted total time left to "targetDate".
+    //Views in console "formattedTimeToLesson".
     console.log(formattedTimeToLesson);
 
-    return formattedTimeToLesson;
+    //Fills "timeLeftElement" with "formattedTimeToLesson".
+    timeLeftElement.innerHTML = formattedTimeToLesson;
 
 }
 
