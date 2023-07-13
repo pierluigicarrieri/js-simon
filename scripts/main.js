eda53a10e529a9857835a0dfa5dd43795184028a
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2023-07-13 16:41:59 +0200

    js commented

--- scripts/main.js
+++ scripts/main.js
@@ -1,35 +1,44 @@
-
-
+//Sets the target date to countdown from.
 const targetDate = new Date(2023, 6, 14, 9, 30);
 
+//Views in consolle "targetDate".
 console.log(targetDate);
 
-setInterval(dateDifference, 1000);
+
+//SetInterval function calls dateDifference function as argument.
+setInterval(dateDifference, 100);
 
 function dateDifference() {
 
+    //Creates a new Date object with the day of now.
     const currentDate = new Date();
 
-    console.log(currentDate);
-
+    //Time difference between dates in milliseconds.
     timeToLesson = targetDate - currentDate;
 
+    /* Next instructions convert the time left in a d:h:m:s format, 
+    step by step. 
+    Integer division ("timeToLesson" / ms in a day) for "daysLeft", 
+    then module operator ("timeToLesson" / ms in a day) for "daysLeftRm" 
+    to calculate "hoursLeft", rinse and repeat till "secondsLeft". */
     const daysLeft = Math.floor(timeToLesson / (24*60*60*1000));
 
-    const daysLeftMs = timeToLesson % (24*60*60*1000);
+    const daysLeftRm = timeToLesson % (24*60*60*1000);
 
-    const hoursLeft =  Math.floor(daysLeftMs / (60*60*1000));
+    const hoursLeft =  Math.floor(daysLeftRm / (60*60*1000));
 
-    const hoursLeftMs =  timeToLesson % (60*60*1000);
+    const hoursLeftRm =  timeToLesson % (60*60*1000);
 
-    const minutesLeft =   Math.floor(hoursLeftMs / (60*1000));
+    const minutesLeft =   Math.floor(hoursLeftRm / (60*1000));
 
-    const minutesLeftMs =   timeToLesson % (60*1000);
+    const minutesLeftRm =   timeToLesson % (60*1000);
 
-    const secondsLeft =    Math.floor(minutesLeftMs / 1000);
+    const secondsLeft =    Math.floor(minutesLeftRm / 1000);
 
-    const formattedTimeToLesson = daysLeft + ":" + hoursLeft  + ":" + minutesLeft + ":" + secondsLeft;
+    //Creates a variable formatting the total time left (d:h:m:s).
+    const formattedTimeToLesson = `${daysLeft}:${hoursLeft}:${minutesLeft}:${secondsLeft}`;
 
+    //Views in consolle the formatted total time left to "targetDate".
     console.log(formattedTimeToLesson);
 
     return formattedTimeToLesson;
